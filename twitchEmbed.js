da5c7f32c3a6a46c49a8171fbf917dd879ac096a
Ray Zuniga <29696391+LiterallyRay@users.noreply.github.com>
2020-11-18 07:16:40 -0800

    Embeding Twitch player

---
+++ twitchEmbed.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+
+1
+new Twitch.Embed("twitch-embed", {
+    width: 1280,
+    height: 720,
+    channel: "LiterallyRay",
+    // only needed if your site is also embedded on embed.example.com and othersite.example.com 
+    //parent: ["embed.example.com", "othersite.example.com"]
+});
