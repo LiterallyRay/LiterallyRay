0d8a0868d212276cf281f3db02419f2f1a641e2d
Ray Zuniga <29696391+LiterallyRay@users.noreply.github.com>
2020-11-18 07:26:49 -0800

    Using Twitch embed code from share button.

--- twitchEmbed.js
+++ twitchEmbed.js
@@ -1,10 +1,1 @@
 // JavaScript source code
-
-1
-new Twitch.Embed("twitch-embed", {
-    width: 1280,
-    height: 720,
-    channel: "LiterallyRay",
-    // only needed if your site is also embedded on embed.example.com and othersite.example.com 
-    //parent: ["embed.example.com", "othersite.example.com"]
-});
